4dd4b89144c5d9d71d314b3b08694dac344f69e8
Yashwant_Sahu <yashwantsahu3002@gmail.com>
2022-04-12 11:50:10 +0530

    Partern Question 1

---
+++ Partern/hollowRect.py
@@ -0,0 +1,18 @@
+
+num = int(input("Enter the number :: "))
+
+for i in range(num):
+    
+    for j in range(num):
+
+        if((i - j) == i or (j - i) == j ):
+            print(f"[{i},{j}]",end ="\t")
+
+        elif (j == num-1 or i == num-1):
+            print(f"[{i},{j}]",end ="\t")
+
+        else :
+            print(" ",end ="\t")
+
+    print('\n')
+
