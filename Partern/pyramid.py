4d1bac4d20b61402089e29bddd1d83f597991e6f
Yashwant_Sahu <yashwantsahu3002@gmail.com>
2022-04-12 17:01:02 +0530

    Correction in pyramid

--- Partern/pyramid.py
+++ Partern/pyramid.py
@@ -2,21 +2,40 @@ import math
 
 num = int(input("Enter the number :: "))
 
+def triangle(num):
+
+    spaces = num - 1
+
+    for i in range(num):
+        
+
+        for j in range(0,spaces):
+            print(end = ' ')
+
+        spaces -= 1
+        
+        for j in range(0,i+1):
+            print('* ',end = "")
+        
+        print("\r")
+
+triangle(num)
+
 # this one is working perfect for odd 
 
-mid = math.floor(num/2)+1
+# mid = math.floor(num/2)+1
 
 # even is not working good
 
 # mid = math.floor(num/2)
 
-print(mid)
-for i in range(mid):
-    mid -= 1
-    for j in range(num-mid):
-        if(j >= mid):
-            print(f"[{i},{j}]",end = '\t')
-        else: 
-            print(f" ",end = '\t')
-    print('\n')
+# print(mid)
+# for i in range(mid):
+#     mid -= 1
+#     for j in range(num-mid):
+#         if(j >= mid):
+#             print(f"[{i},{j}]",end = '\t')
+#         else: 
+#             print(f" ",end = '\t')
+#     print('\n')
 
